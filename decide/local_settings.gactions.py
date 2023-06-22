76704a5802c279aad87b60690c2bff485d179bf9
Fran <fracaralb@alum.us.es>
2023-06-22 11:50:44 +0000

    Deshecho cambio anterior

--- decide/local_settings.gactions.py
+++ decide/local_settings.gactions.py
@@ -30,9 +30,9 @@ APIS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decidedb',
+        'NAME': 'decide',
         'USER': 'decide',
-        'PASSWORD':'complexpassword',
+        'PASSWORD':'decide',
         'HOST': 'localhost',
         'PORT': '5432',
     }
